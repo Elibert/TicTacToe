c60b89f2896a387deb172831e938cf0bcc2c8668
Xhulio Sulce <xsulce@helius-software.com>
2023-08-08 16:55:15 +0200

    GetTeamsByLeague

---
+++ DataSetApi/Controllers/GetDataController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+using System.Net.Http.Headers;
+using DataSetApi.Models;
+using Newtonsoft.Json;
+
+namespace DataSetApi.Controllers
+{
+    public class GetDataController : ApiController
+    {
+        private IConfiguration _config;
+        public GetDataController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async Task<Teams> GetTeamsByLeague(int leagueId)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://"+ _config.GetSection("ApiData:baseUrl").Value+ "/teams/league/" + leagueId),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", _config.GetSection("ApiData:key").Value },
+                    { "X-RapidAPI-Host", _config.GetSection("ApiData:baseUrl").Value },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                var body = await response.Content.ReadAsStringAsync();
+                Teams result = JsonConvert.DeserializeObject<Teams>(body);
+                return result;
+            }
+        }
+    }
+}
